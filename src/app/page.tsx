3fd33f44d10533f69fa251f7f2e039e97f68ad9c
Matheus Amon <amonmatheus757@gmail.com>
2025-08-06 08:28:13 -0300

    feat: add ProductList and ProductItem components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,44 @@
-const Home = () => {
-  return <></>;
+import Image from "next/image";
+
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  console.log(products[0].variants[0].imageUrl);
+
+  return (
+    <div className="space-y-6">
+      <div className="px-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
+
+      <ProductList title="Mais vendidos" products={products} />
+
+      <div className="px-5">
+        <Image
+          src="/banner-02.png"
+          alt="Autêntico"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
